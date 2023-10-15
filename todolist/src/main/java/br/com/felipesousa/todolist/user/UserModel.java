4cefe33b47d88262107f3a01321c555628fecbe9
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-15 09:01:38 -0300

    Criada a tabela 'tb-users' do banco de dados. Funcionando como esperado.

--- todolist/src/main/java/br/com/felipesousa/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/felipesousa/todolist/user/UserModel.java
@@ -1,11 +1,28 @@
 package br.com.felipesousa.todolist.user;
 
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 @Data
+@Entity(name ="tb_users")
 public class UserModel {
   
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
   private String username;
   private String name;
   private String password;
 
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
